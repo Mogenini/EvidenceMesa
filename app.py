74a738d72cbc502303124e3784d6b64a9229a5f5
marceelaaxh <160768521+marceelaaxh@users.noreply.github.com>
2024-11-20 11:48:15 -0600

    Update app.py with bfs

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import mesa
-from mesa.visualization import SolaraViz, make_space_component
-from agents import CarAgent, TrafficLightAgent
-from model import CityModel
-import solara
+from mesa.space import MultiGrid
+import numpy as np
+import seaborn as sns
+from mesa.visualization import Slider, SolaraViz, make_space_component
+import matplotlib.pyplot as plt
+from numpy.matrixlib.defmatrix import matrix
+from solara import component
 
-'''
-Format:
-Semaphore: [ [ [(x,y),True],[] ] ]
-'''
+from agents import CarAgent,TrafficLightAgent
 
 data = {
     "Buildings": [
@@ -30,7 +30,6 @@ data = {
         (18,16),(18,17),(18,20),(18,21),(19,16),(19,21),(20,2),(20,3),(20,5),(20,8),
         (20,9),(20,10),(20,11),(20,16),(20,17),(20,20),(20,21),(21,2),(21,3),(21,4),
         (21,5),(21,8),(21,10),(21,11),(21,16),(21,17),(21,20),(21,21)
-
     ],
     "Parking_Lots": [
         [(2, 3), False], [(9, 2), False], [(11, 4), False], [(6, 5), False],
@@ -57,7 +56,7 @@ data = {
         ["Y", (6,16), (6,22)],
         ["Y", (7,16), (7,22)],
         ["Y", (12,16), (12,22)],
-        ["Y", (13,16), (13,22)],
+        ["Y", (13,15), (13,22)],
         ["X", (2,1), (21,1)],
         ["X", (2,15), (11,15)],
         ["Y", (12,2), (12,15)],
@@ -67,66 +66,93 @@ data = {
         ["Y", (19,2), (19,6)],
         ["X", (17,15), (21,15)],
         ["X", (17,20), (21,20)],
-        ["X", (2,13), (11,13)],
-        ["X", (16,13), (21,13)],
-        ["X",(3,7),(11,7)]
+       # ["X", (2,13), (11,13)],
+        #["X", (16,13), (21,13)],
+        ["X",(3,7),(11,7)],
+        ["Y",(9,2),(9,2)],
+        ["Y",(8,7),(8,8)],
+        ["Y",(19,19),(19,20)],
+        ["Y",(5,7),(5,12)]
     ],
     "Right": [["X",(2,0),(23,0)],["Y",(14,1),(14,12)],["Y",(15,1),(15,21)],
               ["X",(16,6),(20,6)],["Y",(18,7),(18,11)],["Y",(19,7),(19,11)],
-              ["X",(3,6),(11,6)],["X",(2,12),(11,12)],["X",(2,14),(11,14)],
+              ["X",(3,6),(11,6)],["X",(2,12),(11,12)],["X",(4,14),(11,14)],
               ["X",(16,12),(21,12)],["X",(16,14),(21,14)],["Y",(22,0),(22,23)],
               ["Y",(23,0),(23,22)],["X",(2,22),(21,22)],["X",(17,18),(21,18)],
-              ["Y",(14,15),(14,21)]
+              ["Y",(14,15),(14,21)],["Y",(6,5),(6,6)],["Y",(17,17),(17,18)],["Y",(19,17),(19,18)],
+
     ],
-    "Up": [["X",(2,6),(12,6)],["X",(2,7),(12,7)],["Y",(13,2),(13,11)],["Y",(15,2),(15,11)],
+    "Up": [["X",(2,6),(12,6)],["X",(2,7),(12,7)],["Y",(15,2),(15,11)],
            ["Y",(19,3),(19,5)],["Y",(19,8),(19,11)],["Y",(23,2),(23,23)],["X",(1,23),(23,23)],
            ["X",(1,22),(22,22)],["Y",(1,2),(1,21)],["X",(2,15),(22,15)],["X",(16,14),(22,14)],
-           ["X",(2,14),(12,14)],["X",(16,18),(22,18)],["X",(16,19),(22,19)],["Y",(15,16),(15,21)],
-           ["Y",(7,16),(7,21)],["Y",(6,9),(6,12)]
+           ["X",(1,14),(12,14)],["X",(16,18),(22,18)],["X",(16,19),(22,19)],["Y",(15,16),(15,21)],
+           ["Y",(7,16),(7,21)],["Y",(6,9),(6,12)],["Y",(1,3),(2,3)],["X",(19,4),(20,4)],["X",(15,10),(16,10)],
+           ["X",(1,17),(2,17)],["Y",(7,20),(8,20)]
     ],
-    "Down": [["X",(0,0),(22,0)],["X",(0,1),(21,1)],["X",(15,6),(21,6)],["X",(15,7),(21,7)],
+    "Down": [["X",(0,0),(22,0)],["X",(0,1),(22,1)],["X",(15,6),(21,6)],["X",(15,7),(21,7)],
              ["X",(1,12),(21,12)],["X",(1,13),(11,13)],["X",(15,13),(21,13)],["Y",(0,2),(0,21)],
              ["Y",(22,2),(22,21)],["Y",(5,9),(5,11)],["Y",(6,17),(6,21)],["Y",(12,3),(12,10)],
-             ["Y",(12,17),(12,20)],["Y",(14,3),(14,10)],["Y",(14,17),(14,20)],["Y",(18,3),(18,5)],
-             ["Y",(18,9),(18,10)]
+             ["Y",(12,17),(12,20)],["X",(14,1),(15,1)],["X",(14,12),(15,12)],["Y",(18,3),(18,5)],
+             ["Y",(18,9),(18,10)],["X",(11,10),(12,10)],["Y",(11,4),(12,4)],["X",(4,10),(5,10)],
+             ["X",(17,3),(18,3)],["X",(21,9),(22,9)],["X",(5,20),(6,20)],
     ]
 }
-model_params = {
-    "n": 1,
-    "width": 24,
-    "height": 24,
-    "dataStructure": data,
+
+from model import CityModel
+from agents import TrafficLightAgent,CarAgent
+
+proplayer_portal = {"buildingLayer":{"color": "blue","alpha":0.25,"colorbar":False},
+                    "trafficLightLayer":{"color": "orange","alpha":0.25,"colorbar":False},
+                    "parkingLayer":{"color": "yellow","alpha":0.25,"colorbar":False}
 }
 
-model = CityModel(
-    n=model_params["n"],
-    width=model_params["width"],
-    height=model_params["height"],
-    dataStructure=model_params["dataStructure"],
-)
+model_params = {
+    "n": {
+        "type": "SliderInt",
+        "value": 50,
+        "label": "Number of agents:",
+        "min": 1,
+        "max": 3,
+        "step": 1,
+    },
+    "width": 24,
+    "height": 24,
+    "dataStructure":data
+}
 
 def agent_portrayal(agent):
-    if isinstance(agent, TrafficLightAgent):
-        color = "green" if agent.state else "red"
-        return {"size": 20, "color": color, "layer": 2, "shape": "rect"}
-    elif isinstance(agent, CarAgent):
-        color = "tab:blue" if agent.has_parked else "tab:gray"
-        return {"size": 10, "color": color, "layer": 1, "shape": "circle"}
-    else:
-        return {"size": 5, "color": "black", "layer": 0}
+    if isinstance(agent,TrafficLightAgent):
+        size = 50
+        color = "tab:green"
+        shape = "circle"
+    elif isinstance(agent,CarAgent):
+        size = 50
+        color = "tab:red"
+        shape = "circle"
+    return {"size":size,"color":color,"shape":shape}
+    
 
-spaceGraph = make_space_component(agent_portrayal, model.grid.width, model.grid.height)
 
+#Create initial Model Instance
+model = CityModel(1,24,24,data)
+
+# En app.py, después de crear la instancia del modelo:
+print("RightLayer:")
+print(model.grid.properties["RightLayer"].data)
+print("LeftLayer:")
+print(model.grid.properties["LeftLayer"].data)
+print("UpLayer:")
+print(model.grid.properties["UpLayer"].data)
+print("DownLayer:")
+print(model.grid.properties["DownLayer"].data)
+
+
+
+spaceGraph = make_space_component(agent_portrayal,propertylayer_portrayal=proplayer_portal)
 page = SolaraViz(
     model,
-    components=[spaceGraph],
-    model_params=model_params,
-    name="Car Agent and Traffic Light",
+    components = [spaceGraph],
+    model_params = model_params,
+    name = "Car Agent and Traffic Light",
 )
-
-@solara.component
-def MyApp():
-    page.run()
-
-if __name__ == "__main__":
-    solara.run(MyApp)
+page
