93f6c88c75999aa46dc6bf7b65c39d326dc1bea6
EmilianoTavera <110911486+EmilianoTavera@users.noreply.github.com>
2024-11-22 12:16:52 -0600

    Update server.py

--- server.py
+++ server.py
@@ -81,6 +81,10 @@ def positions():
     
     print(positions)
     return jsonify(positions)
+  
+@app.route("/trafficlights")
+def index():
+  return jsonify({"Message": "Hello again"})
 
 if __name__ == "__main__":
     app.run(host ='0.0.0.0', port = 8000, debug=True)
